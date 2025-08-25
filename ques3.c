dc9994a43c7ade8118f6b0aea643a5535230c1fb
Shivang Joshi <shivangjoshi499@gmail.com>
2025-08-25 20:00:15 +0530

    Add files via upload

---
+++ ques3.c
@@ -0,0 +1,13 @@
+//Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+#include<stdio.h>
+
+int main() {
+    float length, breadth;
+    printf("Enter length and breadth of the rectangle: ");
+    scanf("%f %f", &length, &breadth);
+
+    printf("Area: %.2f\n", length * breadth);
+    printf("Perimeter: %.2f\n", 2 * (length + breadth));
+
+    return 0;
+}
